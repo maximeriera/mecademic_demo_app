ff64b3d871f6937f2465f2ff12c0e6dd599256fc
Maxime Riera <maxime.riera@mecademic.com>
2026-03-18 13:10:14 -0400

    feat: Remove obsolete ArduinoBoard, PlanarMotor, and ZaberAxis classes; add new ArduinoBoard implementation

--- devices/__init__.py
+++ devices/__init__.py
@@ -2,8 +2,9 @@ from .Asyril import AsyrilEyePlus
 from .MecaRobot import MecaRobot
 from .Device import Device
 from .PlanarMotor import PlanarMotor
+from .ArduinoBoard import ArduinoBoard
 
 """Devices module for Mecademic demo application."""
 
 
-__all__ = ["AsyrilEyePlus", "MecaRobot", "PlanarMotor", "Device"]
+__all__ = ["AsyrilEyePlus", "MecaRobot", "PlanarMotor", "ArduinoBoard", "Device"]
