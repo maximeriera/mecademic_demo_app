49bcb226e0b8f9c5aa989c833e9f1aadf7afab00
Maxime Riera <maxime.riera@mecademic.com>
2026-04-24 14:20:57 -0400

    Enhance UI and accessibility features
    - Added a new requirements_unix.txt file for dependencies.
    - Updated app.css for a dark theme with improved color contrasts and shadows.
    - Enhanced button styles and hover effects for better user interaction.
    - Improved tab switching functionality in app.js with ARIA attributes for accessibility.
    - Updated message log default text for clarity.
    - Enhanced error and empty state messages in the UI.
    - Improved layout responsiveness in index.html with viewport meta tag and ARIA roles.

--- application_code/home.py
+++ application_code/home.py
@@ -1,9 +1,14 @@
 from devices import Device
 from typing import Dict
 
-import time
+from devices import MecaRobot
 
 def home(devices: Dict[str, Device]):
     """Logic for HOME task."""
-    time.sleep(1)
+    for device_name, device in devices.items():
+        if isinstance(device, MecaRobot.MecaRobot):
+            device.api.SetJointVel(40)
+            device.api.MoveJoints(0, 0, 0, 0, 0, 0)
+            device.logger.info(f"Sent MoveJoints command to {device_name} for zero position.")
+            device.api.WaitIdle()
     return
